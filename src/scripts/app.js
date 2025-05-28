b75750a4145bef686fc33e897dd4a6ed9ed62f56
briac dannequin <briac.dannequin@student.heaj.be>
2025-05-28 08:43:19 +0200

    Nav Js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,19 @@
 "use stick";
 
+let lastScrollTop = 0;
+  const navbar = document.querySelector(".nav_portfolio");
+
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+      // Scroll vers le bas → cacher la nav
+      navbar.style.top = "-100px"; // adapte à la hauteur de ta nav
+    } else {
+      // Scroll vers le haut → montrer la nav
+      navbar.style.top = "0";
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  });
 
